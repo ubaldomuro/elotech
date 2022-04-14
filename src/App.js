97c32012a5ede7981cea3f58bf534465f73dade0
ubaldo <ubaldo.muro@gmail.com>
2022-04-14 17:40:57 -0500

    trying to publish to gh pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ function App() {
   return (
     <div className="container text-center">
       <nav className="navbar bg-light">
-        <div className="container">
-          <a href="#">
-            <img src="images/elotech-logo.png" height="50"></img>
-          </a>
-          <h1>elotech</h1>
-        </div>
+        <a href="#">
+          <img src="images/elotech-logo.png" height="50"></img>
+        </a>
+        <h1>elotech</h1>
       </nav>
       <h2>Pagina en construcción</h2>
-      <img src="images/enginner.gif" width="500"></img>
+      <img src="images/enginner.gif"></img>
       <h2>Por favor regresa el 20 de Abril</h2>
     </div>
   );
